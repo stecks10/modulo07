f3a7915035298db5c5ff47c91611312a355921e0
Vitor Nunes <stecks10@gmail.com>
2021-05-05 19:37:23 -0300

    Estoque na alteração

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -6,30 +6,25 @@ export default function cart(state = [], action) {
       return produce(state, draft => {
         const { product } = action;
 
-        draft.push(product);
+        draft.push(product)
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 
         if (productIndex >= 0) {
-          draft.splice(productIndex, 1);
+          draft.splice(productIndex, 1)
         }
       });
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 
-        if (productIndex >= 0)
+        if (productIndex >= 0) {
           draft[productIndex].amount = Number(action.amount);
-      }
-      );
+        }
+      })
     }
-
     default:
       return state;
   }
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,12 +4,12 @@ import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
-  const productExists = yield select(
-    state => state.cart.find(p => p.id === id),
-  );
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id),
+  )
 
   const stock = yield call(api.get, `/stock/${id}`);
 
@@ -24,22 +24,35 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount))
   } else {
-    const response = yield call(api.get, `/products/${id}`);
+    const response = yield call(api.get, `/products/${id}`)
 
     const data = {
       ...response.data,
       amount: 1,
       priceFormatted: formatPrice(response.data.price),
-    }
+    };
 
     yield put(addToCartSuccess(data));
   }
 }
 
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
 
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque')
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);
